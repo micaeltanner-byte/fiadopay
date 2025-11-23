2d5854f4ba03771cad1c9844ebe3bd8bf8902fbe
CarlaSValente-ads <carla.valente@ucsal.edu.br>
2025-11-23 12:11:34 -0300

    Atualizção do PaymentRequest
    Aprimorar PaymentRequest com anotações do Swagger

--- src/main/java/edu/ucsal/fiadopay/controller/PaymentRequest.java
+++ src/main/java/edu/ucsal/fiadopay/controller/PaymentRequest.java
@@ -1,12 +1,29 @@
 package edu.ucsal.fiadopay.controller;
 
 import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 
 public record PaymentRequest(
-    @NotBlank @Pattern(regexp = "(?i)CARD|PIX|DEBIT|BOLETO") String method,
-    @NotBlank String currency,
-    @NotNull @DecimalMin(value = "0.01") @Digits(integer = 17, fraction = 2) BigDecimal amount,
-    @Min(1) @Max(12) Integer installments,
-    @Size(max = 255) String metadataOrderId
+
+        @Schema(example = "CARD")
+        @NotBlank @Pattern(regexp = "(?i)CARD|PIX|DEBIT|BOLETO")
+        String method,
+
+        @Schema(example = "BRL")
+        @NotBlank
+        String currency,
+
+        @Schema(example = "150.75")
+        @NotNull @DecimalMin(value = "0.01") @Digits(integer = 17, fraction = 2)
+        BigDecimal amount,
+
+        @Schema(example = "3")
+        @Min(1) @Max(12)
+        Integer installments,
+
+        @Schema(example = "PEDIDO-123")
+        @Size(max = 255)
+        String metadataOrderId
+
 ) {}
